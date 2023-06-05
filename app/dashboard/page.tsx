8e418cbc70478b1aba65abeb82aabe77bec130c9
Kerem Esen <kerem.esen.01@hotmail.com>
2023-06-05 05:04:40 +0300

    route error solved

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "@/components/Layout";
 import Table from "@/components/Table";
 import { useAuth } from "@/contexts/AuthContext";
@@ -8,25 +8,29 @@ import { useRouter } from "next/navigation";
 const Dashboard = () => {
   const auth = useAuth();
   const router = useRouter();
+
+  useEffect(() => {
+    if (!auth?.user) {
+      router.push("/");
+    }
+  }, [auth?.user, router]);
+
   if (!auth) {
     return null;
   }
-  if(!router) {
-    return null
-  }
   const { user } = auth;
+
   return (
     <>
-      {user ? (
+      {user && (
         <Layout>
-          <div className=" h-screen w-full  flex items-center justify-center flex-col">
+          <div className="h-screen w-full flex items-center justify-center flex-col">
             <Table />
           </div>
         </Layout>
-      ) : (
-        router.push("/")
       )}
     </>
   );
 };
+
 export default Dashboard;
